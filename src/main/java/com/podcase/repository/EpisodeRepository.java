94326de7ac1b38470c0c3246d31f48fbe535449d
robraymullen <robraymullen@gmail.com>
2022-03-10 23:14:00 +0000

    Remove entity mapping from Subscribed episode Preventing the creation of an unnecessary table.

--- src/main/java/com/podcase/repository/EpisodeRepository.java
+++ src/main/java/com/podcase/repository/EpisodeRepository.java
@@ -5,8 +5,10 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.podcase.model.Episode;
+import com.podcase.model.SubscribedEpisode;
 
 public interface EpisodeRepository extends JpaRepository<Episode, Long> {
 	
@@ -18,4 +20,7 @@ public interface EpisodeRepository extends JpaRepository<Episode, Long> {
 
 	@Query(value="select * from episode where id = (select * from (SELECT episode_id from play_state where user_id = ?1 order by last_played desc limit 1) as subquery)", nativeQuery=true)
 	Optional<Episode> getMostRecentlyPlayed();
+	
+	@Query(name="find_subscribed_episodes", nativeQuery=true)
+	List<SubscribedEpisode> getEpisodesWithPlayState(@Param("podcastId") Long podcastId, @Param("userId")  Long userId);
 }
