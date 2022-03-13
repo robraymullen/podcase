ceeee175459e5cfbad3c9e3820f496865c319073
robraymullen <robraymullen@gmail.com>
2022-03-13 21:33:32 +0000

    Fix missing episode properties (#33)
    * Fix missing episode properties
    Some properties were not being added to the Episode db. Some
    of these included itunes specific foreign markup and the episode description
    
    * Fix tests by setting column definition to LONGTEXT
    *LONGTEXT. Summaries and descriptions could possibly be very long

--- src/main/java/com/podcase/factory/EpisodeFactory.java
+++ src/main/java/com/podcase/factory/EpisodeFactory.java
@@ -1,7 +1,11 @@
 package com.podcase.factory;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+import org.jdom.Content;
+import org.jdom.Element;
 
 import com.podcase.model.Episode;
 import com.sun.syndication.feed.synd.SyndEnclosure;
@@ -18,10 +22,35 @@ public class EpisodeFactory {
 		episode.setPublicationDate(entry.getPublishedDate());
 		episode.setTitle(entry.getTitle());
 		episode.setLink(entry.getLink());
+		episode.setDescription(entry.getDescription().getValue());
 		episode.setFileUrl(((List<SyndEnclosure>)entry.getEnclosures()).get(0).getUrl());
 		episode.setFileType(((List<SyndEnclosure>)entry.getEnclosures()).get(0).getType());
 		episode.setFileLength(((List<SyndEnclosure>)entry.getEnclosures()).get(0).getLength());
 		episode.setGuid(entry.getUri());
+		
+		if (entry.getForeignMarkup() != null) {
+			try {
+				List<Element> foreignElements = (ArrayList) entry.getForeignMarkup();
+				for (Element element : foreignElements) {
+					String name = element.getName();
+					List<Content> contentList = element.getContent();
+					switch (name) {
+						case "keywords":
+							episode.setKeywords(contentList.get(0).getValue());
+							break;
+						case "image":
+							episode.setImageUrl(element.getAttributeValue("href"));
+							break;
+						case "summary":
+							episode.setSummary(contentList.get(0).getValue());
+							break;
+					}
+				}
+			} catch (Exception e) {
+				
+			}
+		}
+		
 		return Optional.of(episode);
 	}
 
--- src/test/java/com/podcase/factory/EpisodeFactoryTest.java
+++ src/test/java/com/podcase/factory/EpisodeFactoryTest.java
@@ -4,6 +4,11 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -21,8 +26,13 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import com.podcase.model.Episode;
+import com.sun.syndication.feed.synd.SyndContent;
 import com.sun.syndication.feed.synd.SyndEnclosure;
 import com.sun.syndication.feed.synd.SyndEntryImpl;
+import com.sun.syndication.feed.synd.SyndFeed;
+import com.sun.syndication.io.FeedException;
+import com.sun.syndication.io.SyndFeedInput;
+import com.sun.syndication.io.XmlReader;
 
 @RunWith(MockitoJUnitRunner.class)
 public class EpisodeFactoryTest {
@@ -34,6 +44,9 @@ public class EpisodeFactoryTest {
 	SyndEnclosure enclosure;
 	
 	@Mock
+	SyndContent content;
+	
+	@Mock
 	List<SyndEnclosure> enclosureList;
 
 	@Before
@@ -41,6 +54,8 @@ public class EpisodeFactoryTest {
 		Date date = new Date();
 		MockitoAnnotations.initMocks(this.getClass());
 		doReturn(date).when(entry).getPublishedDate();
+		doReturn("description").when(content).getValue();
+		doReturn(content).when(entry).getDescription();
 		doReturn("Title").when(entry).getTitle();
 		doReturn("http://link.com").when(entry).getLink();
 		doReturn(enclosureList).when(entry).getEnclosures();
@@ -58,11 +73,12 @@ public class EpisodeFactoryTest {
 	public void testEpisodePropertiesAreSetCorrectly() { 
 		Episode episode = EpisodeFactory.generate(entry).get();
 		
-		assertEquals("Title", entry.getTitle());
+		assertEquals("Title", episode.getTitle());
 		assertEquals("http://link.com", episode.getLink());
 		assertEquals("file://myFile.mp3", episode.getFileUrl());
 		assertEquals("audio/mp3", episode.getFileType());
 		assertEquals(Long.valueOf(12345), episode.getFileLength());
+		assertEquals("description", episode.getDescription());
 	}
 	
 	@Test
@@ -75,6 +91,32 @@ public class EpisodeFactoryTest {
 	public void testThatAnEmptyOptionalIsReturnedWhenEntryHasEmptyEnclosure() {
 		doReturn(new ArrayList<>()).when(entry).getEnclosures();
 		assertEquals(Optional.empty(), EpisodeFactory.generate(entry));
+	}
+	
+	@Test
+	public void testPropertiesFromRssFileWithItunes() throws IllegalArgumentException, FeedException, IOException {
+		URL feedSource = new URL(getFileUrl("sincast.xml"));
+		SyndFeedInput input = new SyndFeedInput();
+		SyndFeed feed = input.build(new XmlReader(feedSource));
+		@SuppressWarnings("unchecked")
+		List<SyndEntryImpl> entries = feed.getEntries();
+		
+		Episode episode = EpisodeFactory.generate(entries.get(0)).get();
+		assertEquals("<div>The SinCast crew dives into the murky waters of TV adaptations this week with Fantasy Island. "
+				+ "Does Michael Pena deliver a suitable Montalban? Are Lucy Hale and Maggie Q underrated and underutilized in film? Are you hoping to hear references to 2000's Bedazzled after you press play? Find out on an all new MINIPOD!!!</div> <div> </div> <div> <div class=\"def\"> <div class=\"yj6qo ajU\"> <div class=\"def\"> <div class=\"def\"> <div> <div class=\"def\"> <div class=\"def\"> <div class=\"def\"> <div class=\"def\"> <div class=\"yj6qo ajU\"> <div class=\"def\"> <div class=\"yj6qo\"> <div class=\"def\"> <div class=\"yj6qo ajU\">We'll be back next Monday for a new episode of SinCast, and keep in touch! Tweet us @cinemasins, comment on SoundCloud, (<a href= \"https://soundcloud.com/cinemasins\">https://soundcloud.com/cinemasins</a>) subscribe to the subreddit (<a href= \"https://www.reddit.com/r/CinemaSins/\">https://www.reddit.com/r/CinemaSins/</a>) and email us at <a href= \"mailto:cinemasinssincast@gmail.com\">cinemasinssincast@gmail.com</a>.</div> </div> </div> </div> </div> </div> </div> </div> </div> "
+				+ "</div> </div> </div> </div> </div> <div class=\"def\"> </div> </div>", episode.getDescription());
+		assertEquals("SinCast - FANTASY ISLAND - Bonus Episode!", episode.getTitle());
+		assertEquals(Long.valueOf(56098793), episode.getFileLength());
+		assertEquals("film,movies,horror,criticism,blumhouse", episode.getKeywords());
+		assertEquals("\"The SinCast crew dives into the murky waters of TV adaptations this week with Fantasy Island. "
+				+ "Does Michael Pena deliver a suitable Montalban? Are Lucy Hale and Maggie Q underrated and underutilized in film? "
+				+ "Are you hoping to hear references "
+				+ "to 2000's Bedazzled after you press play? Find out on an all new MINIPOD!!!\"", episode.getSummary());
+	}
+	
+	private String getFileUrl(String fileName) throws MalformedURLException {
+		File file = new File(getClass().getClassLoader().getResource(fileName).getFile());
+		return file.toURI().toURL().toString();
 	}
 
 }
