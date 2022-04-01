0ce228a9aced9a62d8c882b028e8b25960fe3db7
robraymullen <robraymullen@gmail.com>
2022-04-01 21:37:05 +0100

    Remove dead code in App component Was left over after the header was extracted to a separate component

--- frontend/podcase/src/App.tsx
+++ frontend/podcase/src/App.tsx
@@ -26,51 +26,6 @@ function App() {
     }
   }, []);
 
-  const container = window !== undefined ? () => window.document.body : undefined;
-  const drawerWidth = 240;
-
-  const Search = styled('div')(({ theme }: any) => ({
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(1),
-      width: 'auto',
-    },
-  }));
-
-  const SearchIconWrapper = styled('div')(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }));
-
-  const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: 'inherit',
-    '& .MuiInputBase-input': {
-      padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-      transition: theme.transitions.create('width'),
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        width: '12ch',
-        '&:focus': {
-          width: '20ch',
-        },
-      },
-    },
-  }));
-
   return (
     <BrowserRouter>
       <Box sx={{ display: 'flex' }}>
